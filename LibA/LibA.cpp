b79b07568ba697c89b55926803b7d2da23d51406
pengjungang <pjg202311@gmail.com>
2024-08-29 14:22:39 +0800

    format test

--- LibA/LibA.cpp
+++ LibA/LibA.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <stdio.h>
 
-extern "C" {
-int Add_A(int a, int b) {
-  char buffer[100];
-  sprintf_s(buffer, 100, "call LibA::Add_A : %d + %d = %d", a, b, a + b);
-  std::cout << buffer << std::endl;
-  return a + b;
-}
+extern "C"
+{
+    int Add_A(int a, int b)
+    {
+        char buffer[100];
+        sprintf_s(buffer, 100, "call LibA::Add_A : %d + %d = %d", a, b, a + b);
+        std::cout << buffer << std::endl;
+        return a + b;
+    }
 }
--- LibB/LibB.cpp
+++ LibB/LibB.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <stdio.h>
 
-extern "C" {
-int Add_B(int a, int b) {
-  char buffer[100];
-  sprintf_s(buffer, 100, "call LibB::Add_B : %d + %d = %d", a, b, a + b);
-  std::cout << buffer << std::endl;
-  return a + b;
-}
+extern "C"
+{
+    int Add_B(int a, int b)
+    {
+        char buffer[100];
+        sprintf_s(buffer, 100, "call LibB::Add_B : %d + %d = %d", a, b, a + b);
+        std::cout << buffer << std::endl;
+        return a + b;
+    }
 }
