34395037fd8c96c7e919de4d8e1358fd3fa7db5d
pengjungang <pjg202311@gmail.com>
2024-08-30 17:58:12 +0800

    add debug compile definition

--- src/test.cpp
+++ src/test.cpp
@@ -1,16 +1,31 @@
+#include <Windows.h>
+#include <cstdio>
+#include <iostream>
+
 typedef int (*add_func)(int, int);
 
-#include <Windows.h>
+#ifdef DEBUG
+const char *compile_flag = "Debug";
+#else
+const char *compile_flag = "Release";
+#endif
+
 void TestLibA()
 {
-    auto sharedLib = LoadLibrary("../LibA/a");
+    std::cout << compile_flag << std::endl;
+    char path[100];
+    snprintf(path, 100, "../../LibA/%s/a", compile_flag);
+    auto sharedLib = LoadLibrary(path);
     add_func aAddFunc = (add_func)GetProcAddress(sharedLib, "Add_A");
-    aAddFunc(1234, 234);   
+    aAddFunc(1234, 234);
 }
 
 void TestLibB()
 {
-    auto sharedLib = LoadLibrary("../LibB/b");
+    std::cout << compile_flag << std::endl;
+    char path[100];
+    snprintf(path, 100, "../../LibB/%s/b", compile_flag);
+    auto sharedLib = LoadLibrary(path);
     add_func aAddFunc = (add_func)GetProcAddress(sharedLib, "Add_B");
-    aAddFunc(3434, 129);   
+    aAddFunc(3434, 129);
 }
